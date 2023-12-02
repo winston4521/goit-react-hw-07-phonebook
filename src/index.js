07791227ff5872bb9a4195c760d4c5a80cee4cca
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2023-12-02 11:51:36 +0100

    ss

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import { App } from './components/App/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-07-phonebook">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
